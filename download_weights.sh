75c90e37b0d116924fc7003b2981c59cb971cfb8
Evgeniy <sevostyanikhin02@gmail.com>
2022-03-30 20:10:00 +0300

    final train

--- download_weights.sh
+++ download_weights.sh
@@ -1,2 +1,2 @@
 mkdir weights
-wget --no-check-certificate 'https://drive.google.com/file/d/1VEeKTZlmMXYP8RsnwI4cEjZnyGZkOpVs/view?usp=sharing' -O weights/last.pth
+wget --no-check-certificate 'https://drive.google.com/file/d/1tmQiSyGR0Q26d_ZO--7J_aKJMhr9WHOQ/view?usp=sharing' -O weights/last.pth
